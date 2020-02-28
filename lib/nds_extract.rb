ac564e8b4701555bcdc7c296c074491dd6b65bb0
Karim Macknojia <alishahal@msn.com>
2020-02-28 05:02:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,7 +23,7 @@ def directors_totals(nds)
   director_index = 0
   while director_index < nds.size do
     director = nds[director_index]
-    result[director[:name]] = gross_for_director(nds)
+    result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
   result
